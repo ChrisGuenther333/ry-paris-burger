5be076e28b2b3f5589159185f9a7f487da9de9bd
ChrisGuenther333 <chrisguenther333@gmail.com>
2025-12-06 00:52:32 -0700

    Lots of updates to menu including both mouse hovers and touch start

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,21 @@
-// Mobile Menu Toggle
-let mobileMenuToggle = document.querySelector('button.mobile-menu-toggle')
-let mobileMenu = document.querySelector('ul.mobile-menu')
-
-mobileMenuToggle.addEventListener('click', () => {
+// Mobile menu toggle
+let mobileNav = document.querySelector('div.mobile-nav');
+let mobileMenuToggle = document.querySelector('button.mobile-menu-toggle');
+let mobileMenu = document.querySelector('ul.mobile-menu');
+// Mobile touch
+mobileMenuToggle.addEventListener('touchstart', () => {
     mobileMenu.classList.toggle('mobile-menu-active');
+})
+// Mouse hover/out
+mobileNav.addEventListener('mouseover', () => {
+    if (!mobileMenu.classList.contains('mobile-menu-active')) {
+        mobileMenu.classList.add('mobile-menu-active');
+    }
+})
+mobileNav.addEventListener('mouseout', () => {
+    if (mobileMenu.classList.contains('mobile-menu-active')) {
+        mobileMenu.classList.remove('mobile-menu-active');
+    }
 })
 
 // Save Recipe Button
